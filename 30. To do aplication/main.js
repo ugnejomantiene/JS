84099febfa391d4c6ce3d9798dadc36fc6b0fb5c
ugnejomantiene <upetkeviciute@gmail.com>
2022-12-18 17:24:19 +0200

    Without style done, no comm

--- 30. To do aplication/main.js
+++ 30. To do aplication/main.js
@@ -1,37 +1,68 @@
-class Task {
-    constructor(name) {
-      this.name = name;
-      this.completed = false;
+const container = document.querySelector('.container');
+let inputValue = document.querySelector('.input');
+const add = document.querySelector('.add');
+
+if(window.localStorage.getItem("todos") == undefined){
+     let todos = [];
+     window.localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+let todosEX = window.localStorage.getItem("todos");
+let todos = JSON.parse(todosEX);
+
+
+class Item{
+	constructor(name){
+		this.createItem(name);
+	}
+    createItem(name){
+    	let itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+    	let input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+    	input.classList.add('item_input');
+
+
+    	let remove = document.createElement('button');
+    	remove.classList.add('remove');
+    	remove.innerHTML = "REMOVE";
+    	remove.addEventListener('click', () => this.remove(itemBox, name));
+
+    	container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+		console.log(input.value);
+        itemBox.appendChild(remove);
     }
-  }
-  class ToDoList {
-    constructor() {
-      this.tasks = [];
+  
+    remove(itemBox, name){
+        itemBox.parentNode.removeChild(itemBox);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
     }
-    addTask(task) {
-      this.tasks.push(task);
-    }
-    removeTask(task) {
-      this.tasks = this.tasks.filter(t => t !== task);
-    }
-    displayTasks() {
-      const tasksList = document.getElementById('tasks');
-      tasksList.innerHTML = '';
-      this.tasks.forEach(task => {
-        const li = document.createElement('li');
-        li.textContent = task.name;
-        tasksList.appendChild(li);
-      });
-    }
-  }
-  const toDoList = new ToDoList();
-  const taskForm = document.getElementById('task-form');
-  taskForm.addEventListener('submit', event => {
-    event.preventDefault();
-    const taskName = document.getElementById('task-name').value;
-    if (taskName) {
-      const task = new Task(taskName);
-      toDoList.addTask(task);
-      toDoList.displayTasks();
-    }
-  });
+}
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.value == String){
+		check();
+	}
+})
+function check(){
+	if(inputValue.value != ""){
+		new Item(inputValue.value);
+        todos.push(inputValue.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+		inputValue.value = "";
+	}
+}
+for (let i = 0 ; i < todos.length ; i++){
+    new Item(todos[i]);
+}
+
+
+new Item("Išsimiegoti");
+new Item("Išmokti JS");
